825b712e898753eca3c07b1cd67a25e1f4c93543
Federico Fornari <federico.fornari@cnaf.infn.it>
2023-12-09 21:55:38 +0100

    Updated run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,2 @@
-docker run -it --rm -v $HOME/.ssh:/home/docker/.ssh s3fs-wassh:centos7 bash
+mkdir -p fio_output
+docker run -it --rm -v $HOME/.ssh:/home/docker/.ssh -v $PWD/fio_output:/s3fs-scale-tests/fio_output s3fs-wassh:centos7 bash
